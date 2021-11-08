d9fec105fe64785cf6df988ce92ad1519f211282
carolynegreen <carolynemarie94@gmail.com>
2021-11-08 18:26:10 +0100

    third form javascript

--- main.js
+++ main.js
@@ -7,11 +7,14 @@
 
 const firstStep = document.querySelector(".firstStep");
 const secondStep = document.querySelector(".secondStep");
+const thirdStep = document.querySelector(".thirdStep");
 
 const back1 = document.querySelector("#back1");
 const continue1 = document.querySelector("#continue1");
 const back2 = document.querySelector("#back2");
 const continue2 = document.querySelector("#continue2");
+const back3 = document.querySelector("#back3");
+const continue3 = document.querySelector("#continue3");
 
 continue1.onclick = function () {
   firstStep.style.left = "-65rem";
